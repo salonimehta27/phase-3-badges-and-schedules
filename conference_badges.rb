3b062e26e0588a003569025064dc83b20c726091
Saloni <salonimehta2127@gmail.com>
2021-08-30 18:47:38 -0400

    Completed

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,33 @@
 # Write your code here.
+require 'pry'
+
+list_of_speakers=["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def badge_maker name 
+ "Hello, my name is #{name}."
+end
+
+def batch_badge_creator array 
+ array.map do |name|
+  "Hello, my name is #{name}."
+    end
+end
+batch_badge_creator list_of_speakers
+
+def assign_rooms array
+  array.map.with_index(1) do |name,room|
+    "Hello, my name is #{name}."
+    "Hello, #{name}! You'll be assigned to room #{room}!"
+ end
+end 
+assign_rooms list_of_speakers
+
+def printer array
+ batch_badge_creator(array).each do |badge|
+    puts badge
+ end
+ assign_rooms(array).each do |assignment|
+    puts assignment 
+ end
+end
+printer list_of_speakers
